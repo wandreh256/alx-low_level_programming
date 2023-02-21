bfae2df4c095285c64935f8315845e7a11b810e2
wandreh256 <wandreh2015@gmail.com>
2023-02-21 07:43:01 -0800

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,4 +1,4 @@
-#include main.h
+
 /**
  * print_alphabet - prints the alpha in lowercase
  */
@@ -7,5 +7,6 @@ void print_alphabet(void)
 	char la;
 	for (la = 'a'; la <= 'z'; la++)
 		_putchar(la);
+
 	_putchar('\n');
 }
