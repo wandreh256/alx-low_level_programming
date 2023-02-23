b941458e7975f5142ba7c34a59a2db316815e6de
wandreh256 <wandreh2015@gmail.com>
2023-02-23 01:27:59 -0800

    3-print_numbers.c clear q cd --
    ls
    
    vi 3-print_numbers.c
    git commit -m 3-print_numbers.c

---
+++ 3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - print the number since 0 up to 9
+ * Return: The number since 0 up to 9
+ */
+void print_numbers(void)
+{
+	int x;
+	for (x = 0; x <= 9; x++)
+	{
+	putchar(x + '0');
+	}
+	_putchar('\n');
+}
