c347e1b7002e5b298278206eab9e43384f62f889
wandreh256 <wandreh2015@gmail.com>
2023-03-09 13:48:25 -0800

    puts recursion

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+void_puts_recursion(char*s);
+int_putchar(char c);
+void_print_rev_recursion(char*s);
+int_strlen_recursion(char*s);
+int factial(int n);
+int_pow_recursion(int x, int y);
+int_sqrt_recursion(int n);
+int is_prime_number(int n);
+int is_palidrome(char*s);
+#endif
